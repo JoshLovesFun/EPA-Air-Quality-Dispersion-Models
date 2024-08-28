e956571afb27d390471c8e686132b2734b5fcc1a
JoshLovesFun <ihtftl@gmail.com>
2024-08-28 10:54:17 -0400

    BPIP related files

---
+++ BPIPPRM_Draft/v19191_DRFT/v19191_DRFT_vs_v04274_examples/run_bpipprm.bat
@@ -0,0 +1,5 @@
+..\bpipprm bpipprm_L.inp    bpipprm_04274_L.out    bpipprm_04274_L.sum
+..\bpipprm bpipprm_Rect.inp bpipprm_04274_Rect.out bpipprm_04274_Rect.sum
+
+..\bpipprm_19191_drft_64 bpipprm_L.inp    bpipprm_19191_DRFT_L.out    bpipprm_19191_DRFT_L.sum
+..\bpipprm_19191_drft_64 bpipprm_Rect.inp bpiprmp_19191_DRFT_Rect.out bpipprm_19191_DRFT_Rect.sum
